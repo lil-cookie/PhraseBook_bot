d0f0e30a5d9cb6a4a6b2d3ba8da9a57e4dfb770a
lil-cookie <cookie@ro.ru>
2018-06-17 00:26:22 +0300

    new version

--- index.js
+++ index.js
@@ -1,97 +1,46 @@
 /**
  * Created by Helen on 08.07.2017.
- */
+ 
+process.env["NTBA_FIX_319"] = 1;*/
 var TelegramBot = require('node-telegram-bot-api');
-var token = '446870696:AAFcygEdHjbOGMXxHAquCMzSF_QbzaSBNRE';
-var bot = new TelegramBot(token, {polling: false});
+var token = '446870696:AAHjg7_5VN_hwwP21oIWMvQDZqUWukwVXhU';
+var bot = new TelegramBot(token, {polling: true});
 var request = require("request");
+var langTO='en';
 
-/*костыль для автозапросов к серверу, возвращающих страничку с кодом 200.
-с целью предотвращения падений по таймауту по обращениям к серверу на бесплатном хероку.*/
-// берём Express
-var express = require('express');
-// создаём Express-приложение
-var app = express();
-// создаём маршрут для главной страницы
-app.get('/autoget', function(req, res) {
-    res.sendfile('somepage.html');
-});
-// запускаем сервер на порту 8080
-app.listen(8080);
+var YT_token = 'trnsl.1.1.20170504T180134Z.b9ccf53264e138fd.3d51a5a8a3f7c8b790704622cfb4d1ddf71f1a5b';
+var YT_CommonUrl = 'https://translate.yandex.net/api/v1.5/tr.json';
 
-bot.onText(/\/help (.+)/, function (msg, match) {
-    
-            bot.sendMessage(userId,'команды');
 
+ bot.onText(/\/setlang (.+)/, function (msg, match) {
+    var chatId = msg.from.id;
+    langTO = match[1];
+    console.log(msg);
+    bot.sendMessage(chatId,'Язык на который будет осуществляться перевод изменен на - '+langTO);
 });
 
 
-
-
-
-bot.onText(/\/t (.+)/, function (msg, match) {
+/*send translation of message on any message /(.+)/*/
+bot.onText(/(^(?!\/).+)/, function (msg, match) {
     var userId = msg.from.id;
-    var text = match[1];
+    var text = msg.text;
+    
+    //console.log(text.replace(/(\r\n|\n|\r)/gm," "));
+    //text = text.replace(/(\r\n|\n|\r)/gm," ");
+    //var yaLangListRequest= 'https://translate.yandex.net/api/v1.5/tr/getLangs?key='+YT_token+'&ui='+langTO;
 
-    var yaDetectLangCommonUrl = 'https://translate.yandex.net/api/v1.5/tr.json/detect?key=trnsl.1.1.20170504T180134Z.b9ccf53264e138fd.3d51a5a8a3f7c8b790704622cfb4d1ddf71f1a5b';//&hint=en,ru';
-    var yaTransCommonUrl = 'https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20170504T181631Z.5cffb45738337a99.d503a4cd6b54e2d3c09573b880a6e2803c481583';
+    var yaTransRequest = YT_CommonUrl+'/translate?key='+YT_token+'&text='+encodeURI(text);
+    yaTransRequest+='&lang='+langTO;
+    console.log(langTO+yaTransRequest);
 
-    var yaDetLangRequest = yaDetectLangCommonUrl+'&text='+encodeURI(text);
-    console.log('===='+yaDetLangRequest);
-    var yaTransRequest = yaTransCommonUrl+'&text='+encodeURI(text);
-    lang=null;
-
-    LangReq(yaDetLangRequest,function GetYaTranslate (lang) {
-        if (lang=='ru')
-        {
-            yaTransRequest+='&lang=ru-en';
-            console.log(lang+  yaTransRequest);
-        }
-        else {
-            yaTransRequest+= '&lang=en-ru';
-            console.log( lang+ yaTransRequest);
-        }
-
-        //   bot.sendMessage(userId,text+' - '+lang);
-        TransReq(yaTransRequest,function(def) {
-            //console.log(lang);
-            var message = '';
-
-            def.forEach(function(item) {
-                //var trs =;
-                item.tr.forEach(function(item2) {
-                    message+=item2.text+', ';
-                });
-            });
-            message=message.slice(0,-2)
-            bot.sendMessage(userId,text+' - '+message);
+        TransReq(yaTransRequest,function(translation) {
+            if (translation == '') { bot.sendMessage(userId,'по вашему запросу - \"'+ text +'\" - ничего не нашлось'); } 
+            else {bot.sendMessage(userId,'('+langTO+')'+' '+translation);} 
         });
-    });
-
-
-
 });
 
 
 
-
-
-function LangReq(url, callback){
-    var resp =  request({
-        url: url,
-        json: true
-    },  function getResp (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            console.log('------'+body.lang);
-            callback(body.lang);
-
-        }
-        else  {
-            console.log('++++++'+body.code,body.lang);
-            //bot.sendMessage(userId,'по вашему запросу - '+ text +' - ничего не нашлось(');
-            callback(null);}
-    });
-};
 
 
 function TransReq(url, callback){
@@ -100,10 +49,49 @@ function TransReq(url, callback){
         json: true
     },  function getResp (error, response, body) {
         if (!error && response.statusCode === 200) {
-            callback(body.def);
+            callback(body.text);
         }
         else  {
-            //bot.sendMessage(userId,'по вашему запросу - '+ text +' - ничего не нашлось(');
+            //bot.sendMessage(userId,'по вашему запросу - '+ text +' - ничего не нашлось');
             callback(null);}
     });
 };
+
+
+bot.onText(/\/getlang/, function (msg, match) {
+    var chatId = msg.from.id;
+
+    var yaLangListRequest= YT_CommonUrl+'/getLangs?key='+YT_token+'&ui='+langTO;
+    console.log(yaLangListRequest);
+
+    var message = '';
+    var num = 0;
+
+    LangListReq(yaLangListRequest,function(langs) {
+
+        for (var key in langs) {
+          // этот код будет вызван для каждого свойства объекта
+          // ..и выведет имя свойства и его значение
+          num++;
+          message+= key + " - " + langs[key] +'\n';  
+          console.log( "Код языка: " + key + " Язык: " + langs[key] );
+        }
+           // bot.sendMessage(chatId, message); 
+            bot.sendMessage(chatId, message); 
+        });
+});
+
+function LangListReq(url, callback){
+    var resp =  request({
+        url: url,
+        json: true
+    },  function getResp (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            callback(body.langs);
+        }
+        else  {
+            //bot.sendMessage(userId,'по вашему запросу - '+ text +' - ничего не нашлось');
+            callback(null);}
+    });
+};
+
